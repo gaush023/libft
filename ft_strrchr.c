89cbd18f754904a5a49b017625577bbd83d03893
揚村 周夏 <shuga@yousonnoMacBook-Pro.local>
2023-07-03 20:11:41 +0900

    update

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sagemura <sagemura@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shuga <shuga@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 17:15:01 by sagemura          #+#    #+#             */
-/*   Updated: 2023/07/02 22:26:30 by sagemura         ###   ########.fr       */
+/*   Updated: 2023/07/03 20:09:50 by shuga            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 char	*ft_strrchr(const char *str, int c)
 {
-	size_t	i;
-	char	*temp;
+	ssize_t	len;
 
-	temp = NULL;
-	i = ft_strlen(str);
-	while (i >= 0)
+	len = (ssize_t)ft_strlen(str);
+	while (len >= 0)
 	{
-		if ((unsigned char)str[i] == (unsigned char)c)
-			return ((char *)&str[i]);
-		--i;
+		if ((unsigned char)str[len] == (unsigned char)c)
+		{
+			return ((char *)&str[len]);
+		}
+		len--;
 	}
 	return (NULL);
 }
